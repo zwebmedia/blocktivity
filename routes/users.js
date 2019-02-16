b213f845d45529efdfb32bf68ed50c02961edf6b
Blocktivity <47700846+blocktivity-info@users.noreply.github.com>
2019-02-16 21:57:30 +0100

    Delete users.js

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users', {users: ["Dany", "Jessica", "Albert", "Monica"]});
-});
-
-module.exports = router;
